4bd0e79c76ffb65097c5f90055b31931c6e18628
Kaustubh <kaustubhthakur76@gmail.com>
2025-03-04 01:12:51 +0530

    first project

--- rest_api/main.go
+++ rest_api/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/kaustubhthakur/rest_api/controllers"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
-	fmt.Println("kaustubh")
+
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
+
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongo-url")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
